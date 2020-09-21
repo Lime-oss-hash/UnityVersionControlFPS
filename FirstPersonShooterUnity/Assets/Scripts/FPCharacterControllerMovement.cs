b19812bd2f9fc7fe08b33466a6338656bb9627fc
Sourcecode4777 <65541896+Sourcecode4777@users.noreply.github.com>
2020-09-21 20:33:35 +1200

    Update

--- FirstPersonShooterUnity/Assets/Scripts/FPCharacterControllerMovement.cs
+++ FirstPersonShooterUnity/Assets/Scripts/FPCharacterControllerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class FPCharacterControllerMovement : MonoBehaviour
 {
@@ -10,20 +11,30 @@ public class FPCharacterControllerMovement : MonoBehaviour
     private Vector3 movementDirection;
     private Transform characterTransform;
 
+    private bool isCrouched;
+    private float originHeight;
 
-    public float MovementSpeed;
+    public float SprintingSpeed;
+    public float WalkSpeed;
+
+    public float SprintingSpeedWhenCrouched;
+    public float WalkSpeedWhenCrouched;
+
     public float Gravity = 9.8f;
     public float JumpHeight;
+    public float CrouchHeight = 1f;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
         characterTransform = transform;
+        originHeight = characterController.height;
     }
 
 
     private void Update()
     {
+        float tmp_CurrentSpeed = WalkSpeed;
         if (characterController.isGrounded)
         {
             var tmp_Horizontal = Input.GetAxis("Horizontal");
@@ -35,9 +46,38 @@ public class FPCharacterControllerMovement : MonoBehaviour
             {
                 movementDirection.y = JumpHeight;
             }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                var tmp_CurrentHeight = isCrouched ? originHeight : CrouchHeight;
+                StartCoroutine(DoCrouch(tmp_CurrentHeight));
+                isCrouched = !isCrouched;
+            }
+
+            if (isCrouched)
+            {
+                tmp_CurrentSpeed = Input.GetKey(KeyCode.LeftShift) ? SprintingSpeedWhenCrouched : WalkSpeedWhenCrouched;
+            }
+            else
+            {
+                tmp_CurrentSpeed = Input.GetKey(KeyCode.LeftShift) ? SprintingSpeed : WalkSpeed;
+            }
         }
-        
+
         movementDirection.y -= Gravity * Time.deltaTime;
-        characterController.Move(MovementSpeed * Time.deltaTime * movementDirection);
+        characterController.Move(tmp_CurrentSpeed * Time.deltaTime * movementDirection);
+    }
+
+
+    private IEnumerator DoCrouch(float _target)
+    {
+        float tmp_CurrentHeight = 0;
+        while (Mathf.Abs(characterController.height - _target) > 0.1f)
+        {
+            yield return null;
+            characterController.height =
+                Mathf.SmoothDamp(characterController.height, _target,
+                    ref tmp_CurrentHeight, Time.deltaTime * 5);
+        }
     }
 }
